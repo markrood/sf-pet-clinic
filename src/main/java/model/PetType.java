e945ae7b81a3e52dd5c13b6680102f38181f20e2
mark <markdamonrood@gmail.com>
2018-09-10 13:24:10 -0500

    added pojo

---
+++ src/main/java/model/PetType.java
@@ -0,0 +1,13 @@
+package model;
+
+public class PetType {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
